6e3986cc6b5b205643f6c10b492c17f242a944c4
Lhomme Aymeric <aymericlhomme@orange.fr>
2025-08-16 00:36:58 +0200

    fix(theme)
    - Use new system to initialize components theme

--- src/resources/themes/default/js/admin.js
+++ src/resources/themes/default/js/admin.js
@@ -1,30 +1,28 @@
 const translate = window.translate;
-
 const editor = window.editor;
 const layouts = editor.layouts;
 const fields = editor.fields;
 
-const options = [];
+editor.initializeTheme = function(options) {
+    editor.setOptions(options);
+    const components =
+        [
+            {
+                _id: 'header',
+                label: translate('admin.editor.sidebar.tabs.header'),
+                title: translate('admin.editor.sidebar.tabs.header'),
+                category: translate('admin.editor.category.template'),
+                canEditAppearance: false,
+                fields: [
+                    fields.Repeater('links', {
+                        addLabel: translate('admin.add'),
+                        fields: [...editor.links()],
+                        label: translate('admin.editor.sidebar.tabs.links')
+                    }),
+                ]
+            }
+        ];
 
-
-
-const components =
-    [
-        {
-            _id: 'header',
-            label: translate('admin.editor.sidebar.tabs.header'),
-            title: translate('admin.editor.sidebar.tabs.header'),
-            category: translate('admin.editor.category.template'),
-            canEditAppearance: false,
-            fields: [
-                fields.Repeater('links', {
-                    addLabel: translate('admin.add'),
-                    fields: [...editor.links(options)],
-                    label: translate('admin.editor.sidebar.tabs.links')
-                }),
-            ]
-        }
-    ];
-
-editor.registerComponents(components, options);
-editor.defineElement();
+    editor.registerComponents(components);
+    editor.defineElement();
+};
